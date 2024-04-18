22cd330ccb3c5049bd4c241572da5fa1e503feba
K <Redknclarke@gmail.com>
2024-04-18 07:42:22 -0400

    improved editability and added an easter egg

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import tkinter
 import customtkinter
 import pyautogui
-from PIL import Image, ImageTk
+from PIL import Image
 
-WIDTH = 1280
-HEIGHT = 720
+WIDTH = 1920 // 2
+HEIGHT = 1080 // 2
+QUESTION = "Will I get an A?"
+IMAGE_PATH = "images/a_plus.jpg"
+
 geometry = str(WIDTH) + "x" + str(HEIGHT)
+
 root_tk = customtkinter.CTk()
 root_tk.geometry(geometry)
-root_tk.title("Proposal")
+root_tk.title("I have a question")
+
 customtkinter.set_appearance_mode("System")
 
 
@@ -22,12 +27,21 @@ def defer(e):
 
     deltaX = mouse_x - widget_center_x
     deltaY = mouse_y - widget_center_y
-    pyautogui.moveRel(deltaX, deltaY, 0.75)
+    pyautogui.moveRel(deltaX, deltaY, 0.5)
 
 
 def success():
     frame.destroy()
-    img = customtkinter.CTkImage(Image.open("images/yippee.webp"), size=(WIDTH, HEIGHT))
+    img = customtkinter.CTkImage(Image.open(IMAGE_PATH), size=(WIDTH, HEIGHT))
+    image_label = customtkinter.CTkLabel(root_tk, text="", image=img)
+    image_label.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+
+
+def easter_egg():
+    frame.destroy()
+    img = customtkinter.CTkImage(
+        Image.open("images/easter_egg.jpg"), size=(WIDTH, HEIGHT)
+    )
     image_label = customtkinter.CTkLabel(root_tk, text="", image=img)
     image_label.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
 
@@ -37,7 +51,7 @@ frame.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
 
 label = customtkinter.CTkLabel(
     frame,
-    text="Will you go on a date with me?",
+    text=QUESTION,
     font=customtkinter.CTkFont("Sans Serif", WIDTH // 45),
 )
 
@@ -50,8 +64,10 @@ no_button = customtkinter.CTkButton(
     width=WIDTH / 8,
     height=HEIGHT / 10,
     font=button_font,
+    command=easter_egg,
 )
 no_button.place(relx=0.35, rely=0.75, anchor=tkinter.E)
+no_button.bind("<Motion>", defer)
 no_button.bind("<Enter>", defer)
 
 yes_button = customtkinter.CTkButton(
@@ -64,5 +80,5 @@ yes_button = customtkinter.CTkButton(
     font=button_font,
 )
 yes_button.place(relx=0.65, rely=0.75, anchor=tkinter.W)
-
+pyautogui.FAILSAFE = False
 root_tk.mainloop()
